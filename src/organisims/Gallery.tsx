0a04234e469c8f84f72e3b6614493762c733faba
Christopher Broome <chris.a.broome@gmail.com>
2022-02-09 18:59:08 -0500

    Basic structure

---
+++ src/organisims/Gallery.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from "react";
+import React from "react";
+import styled from "styled-components";
+
+const GalleryWrapper = styled.div``;
+
+export function Gallery() {
+  return (
+    <>
+      <GalleryWrapper>Testing</GalleryWrapper>
+    </>
+  );
+}
